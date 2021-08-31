3eaf005af08725ba2a9f5ac3200c4fb43bc630db
AgentPoles <pauloams16@gmail.com>
2021-08-31 05:46:01 -0700

    footer update

--- src/components/footers/footeradmin.js
+++ src/components/footers/footeradmin.js
@@ -11,7 +11,7 @@ export default function FooterAdmin() {
               <div className="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left">
                 Copyright © {new Date().getFullYear()}{" "}
                 <a
-                  href="#"
+                  style={{ pointerEvents: "none" }}
                   className="text-blueGray-500 hover:text-blueGray-700 text-sm font-semibold py-1"
                 >
                   Paul Oamen
@@ -22,7 +22,7 @@ export default function FooterAdmin() {
               <ul className="flex flex-wrap list-none md:justify-end  justify-center">
                 <li>
                   <a
-                    href=""
+                    href="https://medium.com/@paule.oamen/tensel-6196b6fd1881"
                     className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
                   >
                     Paper
@@ -30,7 +30,7 @@ export default function FooterAdmin() {
                 </li>
                 <li>
                   <a
-                    href=""
+                    style={{ pointerEvents: "none" }}
                     className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
                   >
                     About
@@ -38,7 +38,7 @@ export default function FooterAdmin() {
                 </li>
                 <li>
                   <a
-                    href=""
+                    style={{ pointerEvents: "none" }}
                     className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
                   >
                     Forum
@@ -46,7 +46,7 @@ export default function FooterAdmin() {
                 </li>
                 <li>
                   <a
-                    href=""
+                    style={{ pointerEvents: "none" }}
                     className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
                   >
                     Road map
